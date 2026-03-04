164b080db6176ea89b544dbf92a7788f85fd18fa
FrankHamp <valdemarengelund@gmail.com>
2026-03-04 12:59:29 +0100

    nr 3

--- src/SoundVault/Service/PlaylistManager.java
+++ src/SoundVault/Service/PlaylistManager.java
@@ -5,34 +5,44 @@ public class PlaylistManager {
 
     songs =new ArrayList<>();
 }
-public void addSong(Song song){
+
+public void addSong(Song song) {
     songs.add(song);
 }
-public void displaySongs(){
-    if (songs.isEmpty()){
+
+public void displaySongs() {
+    if (songs.isEmpty()) {
         System.out.println("Playlisten er tom.");
         return;
     }
-    for (Song song : songs){
+    for (Song song : songs) {
         System.out.println(song);
     }
-    public Song findSong(String title){
-        for (Song song : songs){
-            if (song.getTitle().equalsIgnoreCase(title)){
+    public Song findSong (String title){
+        for (Song song : songs) {
+            if (song.getTitle().equalsIgnoreCase(title)) {
                 return song;
             }
         }
         return null;
     }
-    public boolean removeSong(String title){
+    public boolean removeSong (String title){
         Song song = findSong(title);
-        if (song != null){
+        if (song != null) {
             songs.remove(song);
             return true;
         }
-        reurn false;
+        return false;
     }
-    public boolean editSong(String oldtitle, String newTitle){
-        Song
+    public boolean editSong (String oldtitle, String newTitle){
+        Song song = findSong(oldTitle);
+        if (song != null) {
+            song.setTitle(newTitle);
+            return true;
+        }
+        return false;
+    }
+    public void sortSongs () {
+        songs.sort(Comparator.comparing(Song::getTitle));
     }
 }
