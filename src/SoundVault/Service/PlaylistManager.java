7cf8f3d41145803bc6c0b079767ca683d0eae7a0
FrankHamp <valdemarengelund@gmail.com>
2026-03-04 12:40:03 +0100

    nr 2

--- src/SoundVault/Service/PlaylistManager.java
+++ src/SoundVault/Service/PlaylistManager.java
@@ -1,4 +1,38 @@
 package SoundVault.Service;
 
 public class PlaylistManager {
+    private List<Song> songs;
+
+    songs =new ArrayList<>();
+}
+public void addSong(Song song){
+    songs.add(song);
+}
+public void displaySongs(){
+    if (songs.isEmpty()){
+        System.out.println("Playlisten er tom.");
+        return;
+    }
+    for (Song song : songs){
+        System.out.println(song);
+    }
+    public Song findSong(String title){
+        for (Song song : songs){
+            if (song.getTitle().equalsIgnoreCase(title)){
+                return song;
+            }
+        }
+        return null;
+    }
+    public boolean removeSong(String title){
+        Song song = findSong(title);
+        if (song != null){
+            songs.remove(song);
+            return true;
+        }
+        reurn false;
+    }
+    public boolean editSong(String oldtitle, String newTitle){
+        Song
+    }
 }
